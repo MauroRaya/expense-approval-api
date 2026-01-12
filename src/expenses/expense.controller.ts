6e36625da791d0572798d5c2c63b3f57ac1418e8
MauroRaya <maurorayabr@gmail.com>
2026-01-12 20:54:38 -0300

    feat: add expense controller and create expense endpoint

---
+++ src/expenses/expense.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller } from "@nestjs/common";
+import { ExpenseService } from "./expense.service";
+import { Expense } from "./expense.entity";
+import type { CreateExpenseDTO } from "./dto/create-expense.dto";
+import { User } from "src/users/user.entity";
+
+@Controller({ path: 'expense' })
+export class ExpenseController {
+  constructor(private readonly expenseService: ExpenseService) {}
+
+  async createExpense(@Body() dto: CreateExpenseDTO, user: User): Promise<Expense> {
+    return await this.expenseService.createExpense(dto, user);
+  }
+}
