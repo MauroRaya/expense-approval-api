3157e1e148408bf0cbe6797d9ee9e85563aadca1
MauroRaya <maurorayabr@gmail.com>
2026-01-12 20:55:04 -0300

    fix: add expense module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './users/user.module';
+import { ExpenseModule } from './expenses/expense.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { UserModule } from './users/user.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true
     }),
-    UserModule
+    UserModule,
+    ExpenseModule
   ]
 })
 export class AppModule {}
