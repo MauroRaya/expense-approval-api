7464c7fc02d1266e70331d8bb0df2e652a62dac9
MauroRaya <maurorayabr@gmail.com>
2026-01-12 21:31:34 -0300

    feat: add approve expense endpoint for expense controller

--- src/expenses/expense.controller.ts
+++ src/expenses/expense.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Param, Post, Put } from "@nestjs/common";
 import { ExpenseService } from "./expense.service";
 import { Expense } from "./expense.entity";
 import type { CreateExpenseDTO } from "./dto/create-expense.dto";
@@ -11,5 +11,10 @@ export class ExpenseController {
   @Post()
   async createExpense(@Body() dto: CreateExpenseDTO, user: User): Promise<Expense> {
     return await this.expenseService.createExpense(dto, user);
+  }
+
+  @Put(':id')
+  async approveExpense(@Param('id') expenseId: number, user: User): Promise<Expense> {
+    return await this.expenseService.approveExpense(expenseId, user);
   }
 }
