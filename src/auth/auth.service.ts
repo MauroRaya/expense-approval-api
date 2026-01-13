d0ca8befbc88ce3f77109a1d6689617931e29c84
MauroRaya <maurorayabr@gmail.com>
2026-01-13 18:13:29 -0300

    feat: create auth service and sign in method

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { 
+  Injectable, 
+  NotFoundException, 
+  UnauthorizedException 
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/users/user.service';
+import { AccessTokenDTO } from './dto/access-token.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) {}
+
+  async signIn(email: string, password: string): Promise<AccessTokenDTO> {
+    const user = await this.userService.findUserByEmail(email);
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    if (user.password !== password) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { sub: user.id };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload)
+    }
+  }
+}
