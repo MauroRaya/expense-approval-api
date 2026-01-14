5f42d95554edccd73467025fe9b162ce44787fc7
MauroRaya <maurorayabr@gmail.com>
2026-01-13 21:03:14 -0300

    feat: add profile endpoint for auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import type { SignInDTO } from './dto/sign-in.dto';
 import { AccessTokenDTO } from './dto/access-token.dto';
+import { AuthGuard } from './auth.guard';
+import type { Request } from 'express';
 
 @Controller({ path: 'auth' })
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   async signIn(@Body() dto: SignInDTO): Promise<AccessTokenDTO> {
     return await this.authService.signIn(dto.email, dto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  async getProfile(@Req() request: Request) {
+    return request['user'];
   }
 }
