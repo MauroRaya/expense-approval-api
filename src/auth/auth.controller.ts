a73690e93f99eee46e3657ad6c7b76aa105a77dd
MauroRaya <maurorayabr@gmail.com>
2026-01-13 23:13:06 -0300

    refactor: remove unecessary  use guards decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,19 +2,19 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import type { SignInDTO } from './dto/sign-in.dto';
 import { AccessTokenDTO } from './dto/access-token.dto';
-import { AuthGuard } from './auth.guard';
 import type { Request } from 'express';
+import { Public } from 'src/public.decorator';
 
 @Controller({ path: 'auth' })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('login')
   async signIn(@Body() dto: SignInDTO): Promise<AccessTokenDTO> {
     return await this.authService.signIn(dto.email, dto.password);
   }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   async getProfile(@Req() request: Request) {
     return request['user'];
