3143d8805ce14535d6da49ceccd318a57014d0d2
MauroRaya <maurorayabr@gmail.com>
2026-01-13 23:14:12 -0300

    refactor: update routes for new auth guard

--- src/expenses/expense.controller.ts
+++ src/expenses/expense.controller.ts
@@ -1,20 +1,29 @@
-import { Body, Controller, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Param, Post, Put, Req } from "@nestjs/common";
 import { ExpenseService } from "./expense.service";
 import { Expense } from "./expense.entity";
 import type { CreateExpenseDTO } from "./dto/create-expense.dto";
-import { User } from "src/users/user.entity";
+import type { Request } from "express";
+import { JWTPayloadDTO } from "src/auth/dto/jwt-payload.dto";
 
 @Controller({ path: 'expense' })
 export class ExpenseController {
   constructor(private readonly expenseService: ExpenseService) {}
 
   @Post()
-  async createExpense(@Body() dto: CreateExpenseDTO, user: User): Promise<Expense> {
-    return await this.expenseService.createExpense(dto, user);
+  async createExpense(
+    @Body() dto: CreateExpenseDTO, 
+    @Req() request: Request
+  ): Promise<Expense> {
+    const payload = request['user'] as JWTPayloadDTO;
+    return await this.expenseService.createExpense(dto, payload.sub);
   }
 
   @Put(':id')
-  async approveExpense(@Param('id') expenseId: number, user: User): Promise<Expense> {
-    return await this.expenseService.approveExpense(expenseId, user);
+  async approveExpense(
+    @Param('id') expenseId: number, 
+    @Req() request: Request
+  ): Promise<Expense> {
+    const payload = request['user'] as JWTPayloadDTO;
+    return await this.expenseService.approveExpense(expenseId, payload.role);
   }
 }
