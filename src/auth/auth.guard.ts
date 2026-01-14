cc209be597e407969eaaf0e6eead1c1a6a0644a4
MauroRaya <maurorayabr@gmail.com>
2026-01-13 23:12:08 -0300

    refactor: remove auth guard from public scoped methods

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -4,16 +4,37 @@ import {
   Injectable, 
   UnauthorizedException 
 } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { JwtService } from "@nestjs/jwt";
 import { Request } from "express";
+import { IS_PUBLIC_KEY } from "src/public.decorator";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
+    const isPublic = this.reflector.getAllAndOverride(
+      IS_PUBLIC_KEY,
+      [context.getHandler(), context.getClass()]
+    )
+
+    if (isPublic) {
+      return true;
+    }
+
+    const request: Request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader?.startsWith('Bearer ')) {
+      throw new UnauthorizedException();
+    }
+
+    const token = authHeader.slice(7);
+
     if (!token) {
       throw new UnauthorizedException();
     }
@@ -26,10 +47,5 @@ export class AuthGuard implements CanActivate {
     }
 
     return true;
-  }
-
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
   }
 }
